390da74d7f5c4910b0bb0951be95d2939ebe4bc8
Sai Karthik Chandra Enjamuri <enjamurichandra2810@gmail.com>
2025-09-07 07:32:07 +0000

    Binary and Counting Semaphore

--- producerConsumer.cpp
+++ producerConsumer.cpp
@@ -6,37 +6,116 @@
 using namespace std;
 
 mutex m;
-condition_variable cv;
+condition_variable cv_producer,cv_consumer;
 queue<int> buffer;
 const unsigned int maxBufferSize = 10;
 
 void producer(int val){
 	while(val){
 		unique_lock<mutex> ul(m);
-		cv.wait(ul, [](){return buffer.size()<maxBufferSize;});
+		cv_producer.wait(ul, [](){return buffer.size()<maxBufferSize;});
 		buffer.push(val);
 		cout<<"Produced: "<<val<<endl;
 		val--;
 		ul.unlock();
-		cv.notify_one();
+		cv_consumer.notify_one();
 	}
 }
 
 void consumer(){
 	while(true){
 		unique_lock<mutex> ul(m);
-		cv.wait(ul, [](){return buffer.size()>0;});
+		cv_consumer.wait(ul, [](){return buffer.size()>0;});
 		int val = buffer.front();
 		buffer.pop();
 		cout<<"Consumed: "<<val<<endl;
 		ul.unlock();
-		cv.notify_one();
+		cv_producer.notify_one();
 	}
 }
 
+/*-------------------Binary Semaphore---------------------*/
+class binarySemaphore{
+	mutex m;
+	condition_variable cv;
+	bool flag;
+
+	public:
+		binarySemaphore(int val) : flag(val) {}
+
+		void acquire(){
+			unique_lock<mutex> ul(m);
+			cv.wait(ul, [this](){return flag;});
+			flag = false;
+		}
+
+		void release(){
+			{
+				lock_guard<mutex> lg(m);
+				flag = true;
+			}
+			cv.notify_one();
+		}
+};
+/*-------------------Binary Semaphore---------------------*/
+
+/*-------------------Counting Semaphore---------------------*/
+class countingSemaphore{
+	mutex m;
+	condition_variable cv;
+	int count;
+
+	public:
+		countingSemaphore(int val) : count(val) {}
+
+		void acquire(){
+			unique_lock<mutex> ul(m);
+			cv.wait(ul, [this](){return count>0;});
+			--count;
+		}
+
+		void release(){
+			{
+				lock_guard<mutex> lg(m);
+				++count;
+			}
+			cv.notify_one();
+		}
+};
+
+countingSemaphore Empty(10),Full(0);
+mutex m1;
+
+void producerSemaphore(int val){
+	while(val){
+		Empty.acquire();
+		{
+			lock_guard<mutex> lg(m1);
+			buffer.push(val);
+			cout<<"Produced: "<<val<<endl;
+			val--;
+		}
+		Full.release();
+	}
+}
+
+void consumerSemaphore(){
+	while(true){
+		Full.acquire();
+		{
+			lock_guard<mutex> lg(m1);
+			int val = buffer.front();
+			buffer.pop();
+			cout<<"Consumed: "<<val<<endl;
+		}
+		Empty.release();
+	}
+}
+/*-------------------Counting Semaphore---------------------*/
+
 int main(){
-	thread t1(consumer);
-	thread t2(producer,100);
+	thread t1(consumerSemaphore);
+	thread t2(producerSemaphore,100);
 
 	t1.join();
 	t2.join();
